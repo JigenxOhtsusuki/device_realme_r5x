ca43a2b322623ce314ae4d8a70344f832e309a36
JigenxOhtsusuki <abhishekpenkey@gmail.com>
2024-09-17 14:30:06 +0000

    test matrixx

--- device.mk
+++ device.mk
@@ -23,10 +23,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.audio.service \
-    audio.primary.trinket \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
@@ -43,7 +41,6 @@ PRODUCT_PACKAGES += \
     libbatterylistener \
     libcomprcapture \
     libexthwplugin \
-    libhdmipassthru \
     libspkrprot
 
 PRODUCT_COPY_FILES += \
@@ -61,18 +58,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-# A2DP
-PRODUCT_PACKAGES += \
-  liba2dpoffload \
-  android.hardware.bluetooth.a2dp@1.0-impl \
-  android.hardware.bluetooth.a2dp@1.0-service
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
-    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
@@ -117,7 +107,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 # Display
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
@@ -190,9 +179,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti:64 \
     android.hardware.gnss@2.1-service-qti \
     libbatching \
-    libloc_net_iface \
-    libloc_net_iface.vendor \
-    liblocdiagiface \
     libgeofencing \
     libgnss
 
@@ -231,7 +217,6 @@ PRODUCT_COPY_FILES += \
 
 # Init
 PRODUCT_PACKAGES += \
-    fstab.qcom_ramdisk \
     fstab.qcom \
     init.class_main.sh \
     init.oppo.fingerprints.sh \
@@ -242,7 +227,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     set_baseband.sh \
     init.oppo.fingerprints.rc \
-    init.oppo.product.rc \
     init.oppo.reserve.rc \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
@@ -269,7 +253,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    android.hardware.media.omx@1.0-impl \
     android.hardware.media.omx@1.0-service \
     android.hardware.media.c2@1.1.vendor \
     android.hardware.media.c2@1.2.vendor
@@ -348,19 +331,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayR5x \
     TelephonyOverlayR5x \
-    SettingsProviderOverayR5x \
     SettingsOverlayR5x \
     SystemUIOverlayR5x \
     WifiOverlayR5x \
     FrameworksOverlayR5x \
-    TetheringCOverlayR5x \
     BluetoothOverlayR5x \
     ApertureOverlay
 
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.r5x-libperfmgr \
-    android.hardware.power.stats@1.0-service.pixel \
     libqti-perfd-client
 
 # Perf
@@ -505,8 +485,8 @@ PRODUCT_PACKAGES += \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf \
-    android.hardware.wifi.supplicant \
-    android.hardware.wifi.hostapd
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
@@ -547,15 +527,11 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.radio@1.5.vendor \
     android.hardware.secure_element@1.2.vendor \
-    android.hardwareundtrigger@2.3.vendor \
     android.hardware.usb.gadget@1.1.vendor \
     android.hardware.usb@1.2.vendor \
     android.hardware.vibrator@1.3.vendor \
     android.hardware.wifi.hostapd@1.2.vendor \
-    android.hardware.wifi.offload@1.0.vendor \
     android.hardware.wifi.supplicant@1.3.vendor \
     android.hardware.wifi@1.4.vendor \
     android.hidl.allocator@1.0.vendor \
-    android.system.net.netd@1.1.vendor \
-    libadf.vendor \
-    libtinyxml.vendor
+    android.system.net.netd@1.1.vendor
